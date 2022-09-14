0f6330b66a45716da27a8687b726e49a7d9e8f3a
Fernando Azanza <fernandoazanza387@gmail.com>
2022-09-13 22:06:31 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Fernando Azanza on 13/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
